e19266ae6e9a7b6c8c0fce8c5c191290fcaf3539
Sʜɪɴᴇɪ Nᴏᴜᴢᴇɴ <157171073+Shineii86@users.noreply.github.com>
2026-04-04 21:55:02 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,6 @@
  * Vercel serverless entry point.
  * Imports the main Express app and exports it as a serverless function.
  */
-import app from '../src/app.js';
+import app from '../public/index.html';
 
 export default app;
