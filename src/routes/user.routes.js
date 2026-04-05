f1ef09591658803aed6a543244ded6607191160d
Sʜɪɴᴇɪ Nᴏᴜᴢᴇɴ <157171073+Shineii86@users.noreply.github.com>
2026-04-05 09:53:25 +0530

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -12,6 +12,8 @@ router.get('/user/:username', getUserAnalysis);
 router.get('/compare/:user1/:user2', compareUsers);
 router.get('/badge/:username', generateBadge);
 router.get('/card/:username', generateProfileCard);
+router.get('/rank-badge/:username', generateRankBadge);
+router.get('/rank-level/:username', generateRankLevelBadge);
 
 router.get('/', (req, res) => {
   res.json({
@@ -22,6 +24,8 @@ router.get('/', (req, res) => {
       '/api/compare/:user1/:user2',
       '/api/badge/:username',
       '/api/card/:username',
+      'api/rank-badge/:username',
+      'api/rank-level/:username',
     ],
     documentation: 'https://github.com/Shineii86/GitHubAPI',
   });
