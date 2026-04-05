19e314b1f4d2533f69533ee140d64507fe0da124
Sʜɪɴᴇɪ Nᴏᴜᴢᴇɴ <157171073+Shineii86@users.noreply.github.com>
2026-04-05 11:31:52 +0530

    Update user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -13,9 +13,9 @@ const router = express.Router();
 router.get('/user/:username', getUserAnalysis);
 router.get('/compare/:user1/:user2', compareUsers);
 router.get('/badge/:username', generateBadge);
-router.get('/card/:username', generateProfileCard);
+router.get('/vs/:username', generateProfileCard);
 router.get('/rank-badge/:username', generateRankBadge);
-router.get('/rank-level/:username', generateRankLevelBadge);
+router.get('/level-badge/:username', generateRankLevelBadge);
 
 router.get('/', (req, res) => {
   res.json({
@@ -23,11 +23,11 @@ router.get('/', (req, res) => {
     version: '2.0.0',
     endpoints: [
       '/api/user/:username',
-      '/api/compare/:user1/:user2',
+      '/api/vs/:user1/:user2',
       '/api/badge/:username',
       '/api/card/:username',
       'api/rank-badge/:username',
-      'api/rank-level/:username',
+      'api/level-badge/:username',
     ],
     documentation: 'https://github.com/Shineii86/GitHubAPI',
   });
